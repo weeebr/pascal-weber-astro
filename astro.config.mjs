657be3f72535842510d1de573a67d6b4b300d924
Pascal Weber <p.weber.92@gmail.com>
2024-04-21 22:56:51 +0200

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
+import netlify from "@astrojs/netlify";
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [htmx(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify(),
 });
